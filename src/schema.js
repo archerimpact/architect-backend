61e87a980f9a0081b824e9443193bbec14f3e89e
Ali Ahmed <ali.ahmed@berkeley.edu>
2018-06-05 23:32:46 -0700

    updated to reflect comments

--- src/schema.js
+++ src/schema.js
@@ -19,11 +19,9 @@ const userSchema = mongoose.Schema({ // individuals
     },
     name: String,
     email: String,
-    version: {
-        type: Number, // 1 = Community, 2 = Enterprise
-        permissions: [[{ type: mongoose.Schema.Types.ObjectId, ref: 'Team'}, { access: String }]], // permissions can be an id of team (giving a user read/write access. Must specify for each user, it doesn't assume automatic read access for users in an organization).
-        organizationsPartOf: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Organization' }]
-    },
+    version: Number, // 1 = Community, 2 = Enterprise
+    permissions: [[{ type: mongoose.Schema.Types.ObjectId, ref: 'Team'}, { access: String }]], // permissions can be an id of team (giving a user read/write access. Must specify for each user, it doesn't assume automatic read access for users in an organization).
+    organizationsPartOf: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Organization' }],
     trill: Boolean
 });
 
@@ -35,15 +33,12 @@ const teamSchema = mongoose.Schema({ // recursive structure, such that teams can
     _id: mongoose.Schema.Types.ObjectId,
     name: String,
     root: Boolean, // if the team is at the first level of all teams
-    version: {
-        type: Number, // 1 = Community, 2 = Enterprise
-        usersContained: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
-        teamsContained: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Team'}],
-        organizationsPartOf: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Organization'}] // only if enterprise, otherwise null
-    },
+    version: Number, // 1 = Community, 2 = Enterprise
+    usersContained: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
+    teamsContained: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Team'}],
+    organizationsPartOf: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Organization'}], // only if enterprise, otherwise null
     adminIds: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
     created_on: Date,
-    last_modified: Date,
     img: String,
     description: String
 });
@@ -53,7 +48,7 @@ const teamSchema = mongoose.Schema({ // recursive structure, such that teams can
 const organizationSchema = mongoose.Schema({
     name: String,
     teamsContained: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Team'}],
-    users: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+    usersContained: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
     adminIds: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
     permissions: [[{ type: mongoose.Schema.Types.ObjectId, ref: 'Team'}, { access: String }]], // permissions can be an id of team (giving an org read/write access to other teams, perhaps within other organizations). Default is no access.
     description: String,
@@ -72,6 +67,7 @@ const projectSchema = mongoose.Schema({
     data: String, // would hold, links, nodes, and datasets used
     published: Boolean,
     users: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+    teams: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Team' }],
     created_on: Date,
     last_modified: Date,
 })
@@ -81,13 +77,13 @@ const projectSchema = mongoose.Schema({
 const dataSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     type: String,
-    id: String,
+    entityId: String,
     favorited: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
     comments: {
         currentCommentNumber: String,
         commentList: [[{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }, { comment: String}, { order: Number}]]
-    }
-    // later on can add projects here (such that a user can see exciting projects where this dataset was used)
+    },
+    projectId: { type: mongoose.Schema.Types.ObjectId, ref: 'Project' }, // -1 for data layer (if it's not in a project)
 })
 
 exports.User            = mongoose.model('User', userSchema)
@@ -95,4 +91,3 @@ exports.Project         = mongoose.model('Project', projectSchema)
 exports.Data            = mongoose.model('Data', dataSchema)
 exports.Team            = mongoose.model('Team', teamSchema)
 exports.Organization    = mongoose.model('Organization', organizationSchema)
-
